38605e9155f5e49b49699058c937d306dfab9a56
salma-Essam <61875132+salma-Essam@users.noreply.github.com>
2020-03-10 16:46:55 +0200

    section 2
    Co-Authored-By: Yousef Allam <yousef.allam@hotmail.com>

---
+++ Webform1/App_Data/student.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE student
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Firstname] NCHAR(10) NOT NULL, 
+    [lastname] NCHAR(10) NOT NULL, 
+    [Gender] CHAR(1) NOT NULL, 
+    [email] NCHAR(20) NULL, 
+    [address] NVARCHAR(50) NOT NULL, 
+    [phone] NCHAR(11) NOT NULL, 
+    [faculty] NCHAR(10) NOT NULL, 
+    [password] NCHAR(10) NOT NULL, 
+    [confirm] NCHAR(10) NOT NULL, 
+    [nationality] NCHAR(10) NULL, 
+    [birthdate] DATETIME NOT NULL, 
+    [bus] CHAR(1) NULL
+)
